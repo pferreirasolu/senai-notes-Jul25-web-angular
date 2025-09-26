bdd2bee8b52efc31d2dfa8b171dffda634147b1a
Pferreirasolu <pedrohg.ferreira@hotmail.com>
2025-09-25 21:59:38 -0300

    iniciado a note screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginScreen } from './user-module/login-screen/login-screen';
+import { NotesScreen } from './notes-screen/notes-screen';
 
 export const routes: Routes = [
 
@@ -12,8 +13,10 @@ export const routes: Routes = [
         path:"",
         loadComponent: () =>LoginScreen
 
+    },
+        {
+        path:"notes",
+        loadComponent: () =>NotesScreen
+
     }
-
-
-
 ];
---
+++ src/app/notes-screen/notes-screen.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-notes-screen',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './notes-screen.html',
+  styleUrl: './notes-screen.css'
+})
+export class NotesScreen {
+
+}
