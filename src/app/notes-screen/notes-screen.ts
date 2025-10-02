5680604474b29679c5d076d37974b025a1aecc74
pferreirasolu <pedrohg.ferreira@hotmail.com>
2025-10-02 19:25:50 -0300

    Update notes-screen.ts

--- src/app/notes-screen/notes-screen.ts
+++ src/app/notes-screen/notes-screen.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -9,5 +9,34 @@ import { RouterModule } from '@angular/router';
   styleUrl: './notes-screen.css'
 })
 export class NotesScreen {
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
 
+  dataHora: string = '';
+
+  constructor() {
+    const agora = new Date();
+    this.dataHora = agora.toLocaleDateString() + ' ' + agora.toLocaleTimeString();
+  }
+
+  triggerFileInput() {
+    this.fileInput.nativeElement.click();
+  }
+
+  handleImageUpload(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        const telaBanner = document.querySelector('.tela-banner') as HTMLElement;
+        telaBanner.style.backgroundImage = `url('${reader.result}')`;
+        telaBanner.style.backgroundSize = 'cover';
+        telaBanner.style.backgroundPosition = 'center';
+        telaBanner.innerHTML = '';
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
 }
